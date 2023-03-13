cadc6464775f9487fd9acfa6c1b02288d6b52699
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-03-13 19:06:30 +0200

    add installation guide in readme, add candidate model

---
+++ src/Domain/Candidate/Models/Candidate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Domain\Candidate\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    use HasFactory;
+
+}
