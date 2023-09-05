c02981aff97a23be273115281988e6eeacbe7f58
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-09-05 11:34:31 +0300

    wip

--- src/Domain/InterviewManagement/Models/InterviewTemplate.php
+++ src/Domain/InterviewManagement/Models/InterviewTemplate.php
@@ -48,16 +48,6 @@ class InterviewTemplate extends Model
         return $this->morphTo('creator');
     }
 
-    public function questionClusters(): BelongsToMany
-    {
-        return $this->belongsToMany(
-            QuestionCluster::class,
-            'interview_template_questions',
-            'interview_template_id',
-            'question_cluster_id'
-        )->withTimestamps();
-    }
-
     public function questionVariants(): BelongsToMany
     {
         return $this->belongsToMany(
