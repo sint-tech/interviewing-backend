ceeff470b1809853312c9f660b8d00b222b89d46
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-09-28 18:29:45 +0300

    wip

--- src/App/Admin/InvitationManagement/Controllers/InvitationController.php
+++ src/App/Admin/InvitationManagement/Controllers/InvitationController.php
@@ -32,7 +32,7 @@ class InvitationController extends Controller
         $dto = $invitationDataFactory->fromRequest($request);
 
         return InvitationResource::make(
-            $createInvitationAction->execute($dto)->load('interviewTemplate')
+            $createInvitationAction->execute($dto)->load(['interviewTemplate'])
         );
     }
 
