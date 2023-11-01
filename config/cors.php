bcc2154970a544134c219cdad385f5b6390582c2
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-11-01 20:42:08 +0200

    wip

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'admin-api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'admin-api/*', 'organization-api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
