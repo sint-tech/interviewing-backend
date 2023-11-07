bc1cb25596b23ee4beba8a1d6dd99125f1e383bd
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-11-08 01:25:49 +0200

    wip

--- src/App/Organization/InterviewManagement/Requests/InterviewTemplateStoreRequest.php
+++ src/App/Organization/InterviewManagement/Requests/InterviewTemplateStoreRequest.php
@@ -19,10 +19,10 @@ class InterviewTemplateStoreRequest extends FormRequest
             ],
             'description' => ['nullable', 'string', 'min:3', 'max:1000'],
             'reusable' => ['required', 'boolean'],
-            'parent_id' => ['filled', Rule::exists(table_name(InterviewTemplate::class), 'id')
+            'parent_id' => ['filled', Rule::exists(InterviewTemplate::class, 'id')
                 ->withoutTrashed(), // todo only show the interview templates in the organization scope
             ],
-            'availability_status' => ['require  d', Rule::enum(InterviewTemplateAvailabilityStatusEnum::class)],
+            'availability_status' => ['required', Rule::enum(InterviewTemplateAvailabilityStatusEnum::class)],
             'question_variants' => ['required', 'array', 'min:1', Rule::exists(table_name(QuestionVariant::class), 'id')->withoutTrashed()],
         ];
     }
