87a2d588d5a35b1078c52390c2c66a96884e23ec
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-11-07 23:19:48 +0200

    [admin/organization] fix started_at and ended_at format

--- src/App/Admin/Vacancy/Requests/VacancyStoreRequest.php
+++ src/App/Admin/Vacancy/Requests/VacancyStoreRequest.php
@@ -14,8 +14,8 @@ class VacancyStoreRequest extends FormRequest
         return [
             'title' => ['required', 'string', 'min:3', 'max:250'],
             'description' => ['nullable', 'string', 'min:1', 'max:2000'],
-            'started_at' => ['nullable', 'date_format:Y-m-d H:m', 'after:now'],
-            'ended_at' => ['nullable', 'date_format:Y-m-d H:m', 'after:started_at'],
+            'started_at' => ['nullable', 'date_format:Y-m-d H:i', 'after:now'],
+            'ended_at' => ['nullable', 'date_format:Y-m-d H:i', 'after:started_at'],
             'max_reconnection_tries' => ['required', 'min:0', 'max:5'],
             'organization_id' => ['nullable', Rule::exists(table_name(Organization::class), 'id')->withoutTrashed()],
             'open_positions' => ['required', 'integer', 'min:1'],
--- src/App/Organization/Vacancy/Requests/VacancyStoreRequest.php
+++ src/App/Organization/Vacancy/Requests/VacancyStoreRequest.php
@@ -15,8 +15,8 @@ class VacancyStoreRequest extends FormRequest
             'description' => ['nullable', 'min:3', 'max:1000'],
             'max_reconnection_tries' => ['required', 'min:0', 'max:1000'],
             'open_positions' => ['required', 'min:1', 'max:1000'],
-            'started_at' => ['nullable', 'date_format:Y-m-d H:m', 'after:now'],
-            'ended_at' => ['nullable', 'date_format:Y-m-d H:m', 'after:started_at'],
+            'started_at' => ['nullable', 'date_format:Y-m-d H:i', 'after:now'],
+            'ended_at' => ['nullable', 'date_format:Y-m-d H:i', 'after:started_at'],
             'interview_template_id' => ['required', 'int', Rule::exists(table_name(InterviewTemplate::class), 'id')->withoutTrashed()],
             //todo add current trial for interview
         ];
