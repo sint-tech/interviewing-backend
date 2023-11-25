02d4c5eb4b349e8ca9e8e2b9a7707537d6ec28b1
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-11-25 13:38:11 +0200

    wip

--- src/App/Candidate/RegistrationReasons/Resources/RegistrationReasonResource.php
+++ src/App/Candidate/RegistrationReasons/Resources/RegistrationReasonResource.php
@@ -2,15 +2,19 @@
 
 namespace App\Candidate\RegistrationReasons\Resources;
 
+use Domain\Candidate\Models\RegistrationReason;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property RegistrationReason $resource
+ */
 class RegistrationReasonResource extends JsonResource
 {
     public function toArray($request)
     {
         return [
-            'id' => (int) $this->id,
-            'title' => (string) $this->title,
+            'id' => (int) $this->resource->id,
+            'name' => (string) $this->resource->name,
         ];
     }
 }
