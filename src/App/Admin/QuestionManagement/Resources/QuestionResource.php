c2ea388f9bcc2672bfc7d1f684cd5160a58af713
aabadawy <ahmedbadawy.fcai@gmail.com>
2023-12-04 20:55:58 +0200

    add default ai model id to questions

--- src/App/Admin/QuestionManagement/Resources/QuestionResource.php
+++ src/App/Admin/QuestionManagement/Resources/QuestionResource.php
@@ -2,8 +2,12 @@
 
 namespace App\Admin\QuestionManagement\Resources;
 
+use Domain\QuestionManagement\Models\Question;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property Question $resource
+ */
 class QuestionResource extends JsonResource
 {
     public function toArray($request)
@@ -16,6 +20,9 @@ class QuestionResource extends JsonResource
             'difficult_level' => (int) $this->difficult_level,
             'min_reading_duration_in_seconds' => (int) $this->min_reading_duration_in_seconds,
             'max_reading_duration_in_seconds' => (int) $this->max_reading_duration_in_seconds,
+            'default_ai_prompt_id' => $this->resource->default_ai_model_id,
+            'content_prompt' => $this->resource->content_prompt,
+            'system_prompt' => $this->resource->system_prompt,
             'question_cluster' => QuestionClusterResource::make($this->whenLoaded('questionCluster')),
             'question_variants' => QuestionVariantResource::collection($this->whenLoaded('questionVariants')),
             'created_at' => $this->created_at?->format('Y-m-d H:m'),
