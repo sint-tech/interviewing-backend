fd7c3823b277acd5a7341eb16356a8176633e22a
aabadawy <ahmedbadawy.fcai@gmail.com>
2024-02-18 21:05:20 +0200

    wip

--- src/App/Console/Kernel.php
+++ src/App/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendInvitationsCommand;
+use App\Console\Commands\SendRejectionsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +14,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('invitations:send')->everyFiveMinutes();
+        $schedule->command(SendInvitationsCommand::class)->everyFiveMinutes();
+
+        $schedule->command(SendRejectionsCommand::class)->everyFiveMinutes();
     }
 
     /**
