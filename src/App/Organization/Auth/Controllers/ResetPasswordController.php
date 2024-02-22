7381752addac24f6631aecfb2b5be01745bb5ae5
hishamm1 <hishammedhat0@gmail.com>
2024-02-22 15:14:13 +0200

    Add ForgotPasswordLinkController and ResetPasswordController

---
+++ src/App/Organization/Auth/Controllers/ResetPasswordController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Organization\Auth\Controllers;
+
+use Support\Controllers\Controller;
+use Domain\Organization\Models\Employee;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use App\Organization\Auth\Requests\ResetPasswordRequest;
+
+class ResetPasswordController extends Controller
+{
+    public function __invoke(ResetPasswordRequest $request)
+    {
+
+        $status = Password::broker('organizations')->reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (Employee $employee, string $password) {
+                $employee->forceFill([
+                    'password' => Hash::make($password)
+                ]);
+
+                $employee->save();
+                event(new PasswordReset($employee));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? response()->json(['message' => __($status)])
+            : response()->json(['message' => __($status)], 500);
+    }
+}
