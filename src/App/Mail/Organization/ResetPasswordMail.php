72c8c5c72c6e4c6d2222c320b33a61b6b59dc399
hishamm1 <hishammedhat0@gmail.com>
2024-02-22 15:14:52 +0200

    Add reset password email template and mail class

---
+++ src/App/Mail/Organization/ResetPasswordMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail\Organization;
+
+use Domain\InterviewManagement\Models\Interview;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        private string $url
+    ) {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: config('mail.from.address'),
+            subject: "Reset Password"
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.organization.reset-password',
+            with: [
+                'url' => $this->url
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
