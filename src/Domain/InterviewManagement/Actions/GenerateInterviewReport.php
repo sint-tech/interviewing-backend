b7e736445fe7e430b0774b0550407eda592df818
aabadawy <ahmedbadawy.fcai@gmail.com>
2024-02-26 21:33:07 +0200

    Add more keys for interview report
    add new two key values for interview report,
    - first one is language_fluency, which will detect the language fluency rate from 0 to 100
    - save recruiter_advices key which generated by gpt to get the recruiter advices

--- src/Domain/InterviewManagement/Actions/GenerateInterviewReport.php
+++ src/Domain/InterviewManagement/Actions/GenerateInterviewReport.php
@@ -49,6 +49,14 @@ class GenerateInterviewReport
                     'key' => 'question_clusters_stats',
                     'value' => $this->getQuestionClustersStats($interview),
                 ]),
+                ReportValueDto::from([
+                    'key' => 'language_fluency',
+                    'value' => $interview->answers->avg(fn ($answer) => $answer->english_score / 10) * 100,
+                ]),
+                ReportValueDto::from([
+                    'key' => 'recruiter_advices',
+                    'value' => $this->recruiterAdvices($this->getQuestionClustersStats($interview)),
+                ]),
             ],
         ]);
 
@@ -96,9 +104,9 @@ class GenerateInterviewReport
 
     private function getImpacts(array $questionClusters): string
     {
-        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are explaining to the interviewee ther impacts of his scores based on his job profile and the scores from interviewee's report. \n
-                        Generate 3 or 4 impacts in bullet points in html format based on the scores in a professional manner.\n
-                        The interviewee is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while generating the impacts based on the scores from interviewee's report.\n
+        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are explaining to the interviewee ther impacts of his scores based on his job profile and the scores from interviewee's report.
+                        Generate 3 or 4 impacts in bullet points in html format based on the scores in a professional manner.
+                        The interviewee is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while generating the impacts based on the scores from interviewee's report.
                         from interviewee's report scores";
 
         foreach ($questionClusters as $questionClustersStat) {
@@ -122,17 +130,45 @@ class GenerateInterviewReport
 
     protected function getAdvices(array $questionClusters): string
     {
-        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are giving advices based the scores from interviewee's report. \n
-        Generate 3 or 4 Advices in bullet points in html format based on the scores in a professional manner. \n
+        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are giving advices based the scores from interviewee's report.
+        Generate 3 or 4 Advices in bullet points in html format based on the scores in a professional manner.
         The interviewee is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while evaluating the scores from interviewee's report.\n
-        \n from interviewee's report scores
-        \n ------------------------------";
+        from interviewee's report scores
+        ------------------------------";
 
         foreach ($questionClusters as $questionClustersStat) {
             $userContent .= "you got {$questionClustersStat['avg_score']}% at {$questionClustersStat['name']} \n";
         }
 
         $userContent .= 'HR Expert Advices in html format:';
+
+        $response = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => $userContent,
+                ],
+            ],
+        ]);
+
+        return (string) ($response->choices[0])->message->content;
+    }
+
+    protected function recruiterAdvices(array $questionClusters): string
+    {
+        $userContent = "You are an HR Expert, you are giving advices to junior recruiter about a candidate the junior recruiter wants to hire.
+        give advices based the candidate scores report.
+        Generate 3 or 4 Advices in bullet point based on the scores in a professional manner.
+        The candidate is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while evaluating the scores from candidate report.
+        candidate scores report
+        ------------------------------";
+
+        foreach ($questionClusters as $questionClustersStat) {
+            $userContent .= "you got {$questionClustersStat['avg_score']}% at {$questionClustersStat['name']} \n";
+        }
+
+        $userContent .= 'HR Expert Advices to the junior recruiter:';
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
