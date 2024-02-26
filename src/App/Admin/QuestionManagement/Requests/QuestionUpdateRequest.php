a229cf29d1b937c7b2b3662fe4d3d0fa233402df
HishamM1 <hishammedhat0@gmail.com>
2024-02-26 19:50:53 +0200

    Add AI prompt validation rules to QuestionUpdateRequest.php

--- src/App/Admin/QuestionManagement/Requests/QuestionUpdateRequest.php
+++ src/App/Admin/QuestionManagement/Requests/QuestionUpdateRequest.php
@@ -7,6 +7,10 @@ use Domain\QuestionManagement\Models\Question;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Enum;
+use Domain\AiPromptMessageManagement\Enums\AiModelEnum;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
 
 class QuestionUpdateRequest extends FormRequest
 {
@@ -20,11 +24,34 @@ class QuestionUpdateRequest extends FormRequest
             'question_type' => ['filled', (new Enum(QuestionTypeEnum::class))],
             'min_reading_duration_in_seconds' => ['filled', 'integer', 'min:1', 'lt:max_reading_duration_in_seconds'],
             'max_reading_duration_in_seconds' => ['filled', 'integer', 'min:1', 'gt:min_reading_duration_in_seconds'],
+            'ai_prompt' => ['filled', 'array', 'min:1'],
+            'ai_prompt.model' => ['filled', 'distinct', Rule::enum(AiModelEnum::class)],
+            'ai_prompt.content' => [
+                'filled', 'string',
+                function ($attribute, $value, \Closure $fail) {
+                    if ($this->aiModelsPlaceholdersMissing($value, $placeholders = ['_QUESTION_TEXT_', '_INTERVIEWEE_ANSWER_'])) {
+                        $fail('string should contains all these terms: ' . Arr::join($placeholders, ', ', 'and '));
+                    }
+                },
+            ],
+            'ai_prompt.system' => [
+                'string',
+                function ($attribute, $value, \Closure $fail) {
+                    if ($this->aiModelsPlaceholdersMissing($value, $placeholders = ['_RESPONSE_JSON_STRUCTURE_'])) {
+                        $fail('string should contain' . Arr::join($placeholders, ', ', 'and '));
+                    }
+                },
+            ],
         ];
     }
 
     public function question(): Question
     {
         return $this->route('question');
+    }
+
+    private function aiModelsPlaceholdersMissing(string $value, array $placeholders): bool
+    {
+        return !Str::contains($value, $placeholders);
     }
 }
