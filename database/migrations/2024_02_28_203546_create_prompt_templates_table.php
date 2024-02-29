ca968867de8d7e64fc4d7a22668479bf5f85524d
HishamM1 <hishammedhat0@gmail.com>
2024-03-01 00:04:28 +0200

    Add migration for creating prompt_templates table

---
+++ database/migrations/2024_02_28_203546_create_prompt_templates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prompt_templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('text');
+            $table->text('stats_text');
+            $table->text('conclusion_text');
+            $table->unsignedTinyInteger('version')->default(1);
+            $table->boolean('is_selected')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prompt_templates');
+    }
+};
