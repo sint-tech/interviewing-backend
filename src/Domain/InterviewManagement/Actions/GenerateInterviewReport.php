ed3248df1188447b40fa22d631b0aadf4dbb4ad2
HishamM1 <hishammedhat0@gmail.com>
2024-03-01 00:14:36 +0200

    Refactor interview report generation code

--- src/Domain/InterviewManagement/Actions/GenerateInterviewReport.php
+++ src/Domain/InterviewManagement/Actions/GenerateInterviewReport.php
@@ -2,6 +2,8 @@
 
 namespace Domain\InterviewManagement\Actions;
 
+use Domain\AiPromptMessageManagement\Enums\PromptTemplateVariableEnum;
+use Domain\AiPromptMessageManagement\Models\PromptTemplate;
 use Domain\InterviewManagement\Exceptions\InterviewNotFinishedException;
 use Domain\InterviewManagement\Models\Interview;
 use Domain\InterviewManagement\ValueObjects\InterviewReportValueObject;
@@ -105,23 +107,36 @@ class GenerateInterviewReport
 
     private function getImpacts(array $questionClusters): string
     {
-        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are explaining to the interviewee ther impacts of his scores based on his job profile and the scores from interviewee's report.
-                        Generate 3 or 4 impacts in bullet points in html format based on the scores in a professional manner.
-                        The interviewee is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while generating the impacts based on the scores from interviewee's report.
-                        from interviewee's report scores";
+        $impact_template = PromptTemplate::query()
+            ->where('name', 'impact')
+            ->orderByDesc('is_selected')
+            ->orderByDesc('version')
+            ->firstOrFail();
+
+        $impact_template_content = str_replace(
+            [PromptTemplateVariableEnum::JobTitle->value],
+            [$this->interview->vacancy->interviewTemplate->jobTitle->title],
+            $impact_template->text
+        );
 
         foreach ($questionClusters as $questionClustersStat) {
-            $userContent .= "you got {$questionClustersStat['avg_score']}% at {$questionClustersStat['name']} \n";
+            $impact_template_content .= str_replace([
+                PromptTemplateVariableEnum::QuestionClusterName->value,
+                PromptTemplateVariableEnum::QuestionClusterAvgScore->value,
+            ], [
+                $questionClustersStat['name'],
+                $questionClustersStat['avg_score'],
+            ], $impact_template->stats_text);
         }
 
-        $userContent .= 'impacts in bullets point in html format:';
+        $impact_template_content .= $impact_template->conclusion_text;
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
                 [
                     'role' => 'user',
-                    'content' => $userContent,
+                    'content' => $impact_template_content,
                 ],
             ],
         ]);
@@ -131,24 +146,36 @@ class GenerateInterviewReport
 
     protected function getCandidateAdvices(array $questionClusters): string
     {
-        $userContent = "You are an HR Expert, and an interviewee gave you the report they got from, you are giving advices based the scores from interviewee's report.
-        Generate 3 or 4 Advices in bullet points in html format based on the scores in a professional manner.
-        The interviewee is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while evaluating the scores from interviewee's report.\n
-        from interviewee's report scores
-        ------------------------------";
+        $candidate_template = PromptTemplate::query()
+            ->where('name', 'candidate_advices')
+            ->orderByDesc('is_selected')
+            ->orderByDesc('version')
+            ->firstOrFail();
+
+        $candidate_template_content = str_replace(
+            [PromptTemplateVariableEnum::JobTitle->value],
+            [$this->interview->vacancy->interviewTemplate->jobTitle->title],
+            $candidate_template->text
+        );
 
         foreach ($questionClusters as $questionClustersStat) {
-            $userContent .= "you got {$questionClustersStat['avg_score']}% at {$questionClustersStat['name']} \n";
+            $candidate_template_content .= str_replace([
+                PromptTemplateVariableEnum::QuestionClusterName->value,
+                PromptTemplateVariableEnum::QuestionClusterAvgScore->value,
+            ], [
+                $questionClustersStat['name'],
+                $questionClustersStat['avg_score'],
+            ], $candidate_template->stats_text);
         }
 
-        $userContent .= 'HR Expert Advices in html format:';
+        $candidate_template_content .= $candidate_template->conclusion_text;
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
                 [
                     'role' => 'user',
-                    'content' => $userContent,
+                    'content' => $candidate_template_content,
                 ],
             ],
         ]);
@@ -158,25 +185,36 @@ class GenerateInterviewReport
 
     protected function getRecruiterAdvices(array $questionClusters): string
     {
-        $userContent = "You are an HR Expert, you are giving advices to junior recruiter about a candidate the junior recruiter wants to hire.
-        give advices based the candidate scores report.
-        Generate 3 or 4 Advices in bullet point based on the scores in a professional manner.
-        The candidate is applying for {$this->interview->vacancy->interviewTemplate->jobTitle->title}, take that into consideration while evaluating the scores from candidate report.
-        candidate scores report
-        ------------------------------";
+        $recruiter_template = PromptTemplate::query()
+            ->where('name', 'recruiter_advices')
+            ->orderByDesc('is_selected')
+            ->orderByDesc('version')
+            ->firstOrFail();
+
+        $recruiter_template_content = str_replace(
+            [PromptTemplateVariableEnum::JobTitle->value],
+            [$this->interview->vacancy->interviewTemplate->jobTitle->title],
+            $recruiter_template->text
+        );
 
         foreach ($questionClusters as $questionClustersStat) {
-            $userContent .= "you got {$questionClustersStat['avg_score']}% at {$questionClustersStat['name']} \n";
+            $recruiter_template_content .= str_replace([
+                PromptTemplateVariableEnum::QuestionClusterName->value,
+                PromptTemplateVariableEnum::QuestionClusterAvgScore->value,
+            ], [
+                $questionClustersStat['name'],
+                $questionClustersStat['avg_score'],
+            ], $recruiter_template->stats_text);
         }
 
-        $userContent .= 'HR Expert Advices to the junior recruiter:';
+        $recruiter_template_content .= $recruiter_template->conclusion_text;
 
         $response = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
                 [
                     'role' => 'user',
-                    'content' => $userContent,
+                    'content' => $recruiter_template_content,
                 ],
             ],
         ]);
