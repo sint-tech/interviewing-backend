3c64a4a34113bd429d1618744cc0b03d72525377
HishamM1 <hishammedhat0@gmail.com>
2024-03-07 03:11:12 +0200

    Add condition to include public question variants

--- src/Domain/QuestionManagement/Builders/QuestionVariantBuilder.php
+++ src/Domain/QuestionManagement/Builders/QuestionVariantBuilder.php
@@ -6,12 +6,14 @@ use Domain\Organization\Models\Employee;
 use Domain\Users\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Domain\InterviewManagement\Models\InterviewTemplate;
+use Domain\QuestionManagement\Enums\QuestionVariantStatusEnum;
 
 class QuestionVariantBuilder extends Builder
 {
     public function forOrganizationEmployee(Employee $employee): self
     {
-        return $this->where('organization_id', $employee->organization_id);
+        return $this->where('organization_id', $employee->organization_id)
+            ->orWhere('status', QuestionVariantStatusEnum::Public);
     }
 
     public function forSintAdmin(User $sintAdmin): self
