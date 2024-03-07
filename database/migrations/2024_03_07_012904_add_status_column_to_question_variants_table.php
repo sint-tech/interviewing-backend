a902f4fe86e29c26865abed9f17bf0981d40f55f
HishamM1 <hishammedhat0@gmail.com>
2024-03-07 02:59:38 +0200

    Add status column to question_variants table migration

---
+++ database/migrations/2024_03_07_012904_add_status_column_to_question_variants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Domain\QuestionManagement\Enums\QuestionVariantStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('question_variants', function (Blueprint $table) {
+            $table->unsignedTinyInteger('status')
+                ->default(QuestionVariantStatusEnum::Private)
+                ->after('question_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('question_variants', function (Blueprint $table) {
+            //
+        });
+    }
+};
