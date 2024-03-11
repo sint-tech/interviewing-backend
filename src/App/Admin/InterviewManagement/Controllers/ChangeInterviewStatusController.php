6efa6026e3fbbf1d2a733615e4b470d37d019691
HishamM1 <hishammedhat0@gmail.com>
2024-03-12 00:47:49 +0200

    Refactor ChangeInterviewStatusController to use ChangeInterviewStatusAction

--- src/App/Admin/InterviewManagement/Controllers/ChangeInterviewStatusController.php
+++ src/App/Admin/InterviewManagement/Controllers/ChangeInterviewStatusController.php
@@ -4,19 +4,16 @@ namespace App\Admin\InterviewManagement\Controllers;
 
 use App\Admin\InterviewManagement\Requests\ChangeInterviewStatusRequest;
 use App\Admin\InterviewManagement\Resources\InterviewResource;
+use Domain\InterviewManagement\Actions\ChangeInterviewStatusAction;
 use Domain\InterviewManagement\Models\Interview;
 use Support\Controllers\Controller;
 
 class ChangeInterviewStatusController extends Controller
 {
-    public function __invoke(Interview $interview, ChangeInterviewStatusRequest $request): InterviewResource
+    public function __invoke(Interview $interview, ChangeInterviewStatusRequest $request, ChangeInterviewStatusAction $action): InterviewResource
     {
-        $interview->update([
-            'status' => $request->validated('status'),
-        ]);
-
         return InterviewResource::make(
-            $interview->refresh()->load('candidate')
+            $action->execute($interview, $request->validated('status'))
         );
     }
 }
