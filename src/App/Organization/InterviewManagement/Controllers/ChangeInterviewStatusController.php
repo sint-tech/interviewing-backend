1e46f7320178509c524391a489fc5aa88b95390f
HishamM1 <hishammedhat0@gmail.com>
2024-03-12 00:47:55 +0200

    Add ChangeInterviewStatusController

---
+++ src/App/Organization/InterviewManagement/Controllers/ChangeInterviewStatusController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Organization\InterviewManagement\Controllers;
+
+use App\Admin\InterviewManagement\Requests\ChangeInterviewStatusRequest;
+use App\Admin\InterviewManagement\Resources\InterviewResource;
+use Domain\InterviewManagement\Actions\ChangeInterviewStatusAction;
+use Domain\InterviewManagement\Models\Interview;
+use Support\Controllers\Controller;
+
+class ChangeInterviewStatusController extends Controller
+{
+    public function __invoke(Interview $interview, ChangeInterviewStatusRequest $request, ChangeInterviewStatusAction $action): InterviewResource
+    {
+        return InterviewResource::make(
+            $action->execute($interview, $request->validated('status'))
+        );
+    }
+}
