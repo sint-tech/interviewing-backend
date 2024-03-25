2d98a6f401fe164e99d67940004529fe555c7415
HishamM1 <hishammedhat0@gmail.com>
2024-03-25 04:23:31 +0200

    Fix date formatting in VacancyResource.php

--- src/App/Candidate/InterviewManagement/Resources/VacancyResource.php
+++ src/App/Candidate/InterviewManagement/Resources/VacancyResource.php
@@ -15,7 +15,7 @@ class VacancyResource extends JsonResource
         return [
             'id' => $this->resource->id,
             'title' => $this->resource->title,
-            'ended_at' => $this->resource->ended_at,
+            'ended_at' => $this->resource->ended_at?->format('Y-m-d H:i'),
             'description' => $this->resource->descripion,
             'interview_template_id' => $this->resource->interview_template_id,
             'organization' => OrganizationResource::make($this->whenLoaded('organization')),
