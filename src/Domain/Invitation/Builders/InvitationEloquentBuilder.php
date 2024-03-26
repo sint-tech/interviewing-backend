261b9ab3752d8122f4656c1bea042c3c3bd72ab2
HishamM1 <hishammedhat0@gmail.com>
2024-03-26 20:13:29 +0200

    Refactor whereIsNotExpired method to include null expired_at values

--- src/Domain/Invitation/Builders/InvitationEloquentBuilder.php
+++ src/Domain/Invitation/Builders/InvitationEloquentBuilder.php
@@ -20,7 +20,7 @@ class InvitationEloquentBuilder extends Builder
     public function whereIsNotExpired(): self
     {
         return $this->whereHas('vacancy', fn ($query) => $query->where('ended_at', '>', now()))
-            ->where('expired_at', '>', now())
+            ->where(fn ($query) => $query->where('expired_at', '>', now())->orWhereNull('expired_at'))
             ->whereNull('used_at');
     }
 }
