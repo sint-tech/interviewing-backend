1d992efc9469d93f16bdadcf0b4392de451fbcf8
HishamM1 <hishammedhat0@gmail.com>
2024-03-26 19:56:37 +0200

    Wip

--- src/Domain/Invitation/Builders/InvitationEloquentBuilder.php
+++ src/Domain/Invitation/Builders/InvitationEloquentBuilder.php
@@ -13,12 +13,14 @@ class InvitationEloquentBuilder extends Builder
     public function whereIsExpired(): self
     {
         return $this->whereHas('vacancy', fn ($query) => $query->where('ended_at', '<', now()))
-            ->orWhere(fn ($query) => $query->where('expired_at', '<', now())->orWhereNotNull('used_at'));
+            ->orWhere('expired_at', '<', now())
+            ->orWhereNotNull('used_at');
     }
 
     public function whereIsNotExpired(): self
     {
         return $this->whereHas('vacancy', fn ($query) => $query->where('ended_at', '>', now()))
-            ->where(fn ($query) => $query->where('expired_at', '>', now())->orWhereNull('used_at'));
+            ->where('expired_at', '>', now())
+            ->whereNull('used_at');
     }
 }
