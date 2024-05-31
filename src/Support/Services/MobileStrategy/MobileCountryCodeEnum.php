ee0aca38cbe7e835e928a45303ab1fe841d75e8e
HishamM1 <hishammedhat0@gmail.com>
2024-05-31 21:42:09 +0300

    MobileCountryCodeEnum include getValues() method

--- src/Support/Services/MobileStrategy/MobileCountryCodeEnum.php
+++ src/Support/Services/MobileStrategy/MobileCountryCodeEnum.php
@@ -7,4 +7,12 @@ enum MobileCountryCodeEnum: string
     case Egypt = '+20';
 
     case Saudi = '+966';
+
+    public static function getValues(): array
+    {
+        return [
+            self::Egypt->value,
+            self::Saudi->value,
+        ];
+    }
 }
