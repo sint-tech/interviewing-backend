d2115aa2ca566746bbd7c793d64ec4be5837d39b
HishamM1 <85462680+HishamM1@users.noreply.github.com>
2024-06-28 03:51:34 +0300

    Revert "Add ai model"

--- src/Domain/AiPromptMessageManagement/Enums/AiModelEnum.php
+++ src/Domain/AiPromptMessageManagement/Enums/AiModelEnum.php
@@ -2,16 +2,7 @@
 
 namespace Domain\AiPromptMessageManagement\Enums;
 
-enum AiModelEnum: int
+enum AiModelEnum: string
 {
-    case Gpt_3_5 = 1;
-    case Gpt_4o = 2;
-
-    public static function getValues(): array
-    {
-        return [
-            self::Gpt_3_5->value,
-            self::Gpt_4o->value,
-        ];
-    }
+    case Gpt_3_5 = 'gpt-3.5-turbo';
 }
