21b987e5bc9ee6f3e3f9838113786ad9dc1398b9
HishamM1 <hishammedhat0@gmail.com>
2024-07-05 17:07:03 +0300

    install bugsnag

--- config/app.php
+++ config/app.php
@@ -193,7 +193,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         /*
          * Application Service Providers...
          */
