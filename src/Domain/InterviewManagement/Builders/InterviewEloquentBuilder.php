ef07d196e7616bd1963318a5a143957425740008
HishamM1 <hishammedhat0@gmail.com>
2024-07-14 02:54:59 +0300

    wip

--- src/Domain/InterviewManagement/Builders/InterviewEloquentBuilder.php
+++ src/Domain/InterviewManagement/Builders/InterviewEloquentBuilder.php
@@ -99,7 +99,7 @@ class InterviewEloquentBuilder extends Builder
     {
         Log::info('vacancy_id', [$vacancy_id]);
         $selected = Interview::where('vacancy_id', $vacancy_id)->whereSelected()->select('id')->get();
-        Log::info('selected', $selected);
+        Log::info('selected', [$selected]);
         $remaining_positions = $open_positions - $selected->count();
 
         if ($remaining_positions <= 0) {
@@ -107,10 +107,10 @@ class InterviewEloquentBuilder extends Builder
         }
 
         $passed = Interview::where('vacancy_id', $vacancy_id)->wherePassed()->take($remaining_positions)->select('id')->get();
-        Log::info('passed', $passed);
+        Log::info('passed', [$passed]);
 
         $accepted = $passed->merge($selected);
-        Log::info('accepted', $accepted);
+        Log::info('accepted', [$accepted]);
 
         return $this->whereIn('id', $accepted->pluck('id'));
     }
