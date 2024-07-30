37ebb16714e52bf309f35f914db1c9447fc6dd9f
HishamM1 <hishammedhat0@gmail.com>
2024-07-30 22:12:25 +0300

    make mobile nullable

---
+++ database/migrations/2024_07_28_220326_make_columns_nullable_in_invitations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->string('mobile_country_code')->nullable()->change();
+            $table->integer('mobile_number')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->string('mobile_country_code')->change();
+            $table->integer('mobile_number')->change();
+        });
+    }
+};
