cfd33b043525b6b3fa979c0640c6ef00ecd4937f
HishamM1 <hishammedhat0@gmail.com>
2024-07-30 22:14:09 +0300

    add external invite request to dto

--- src/App/Organization/InvitationManagement/Factories/InvitationDtoFactory.php
+++ src/App/Organization/InvitationManagement/Factories/InvitationDtoFactory.php
@@ -2,9 +2,11 @@
 
 namespace App\Organization\InvitationManagement\Factories;
 
+use App\Candidate\Invitation\Requests\ExternalInviteRequest;
 use App\Organization\InvitationManagement\Requests\InvitationStoreRequest;
 use Domain\Invitation\DataTransferObjects\InvitationDto;
 use Domain\Vacancy\Models\Vacancy;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 
 class InvitationDtoFactory
@@ -12,7 +14,8 @@ class InvitationDtoFactory
     public function fromRequest(Request $request): InvitationDto
     {
         return match ($request::class) {
-            InvitationStoreRequest::class => $this->fromInvitationStoreRequest($request)
+            InvitationStoreRequest::class => $this->fromInvitationStoreRequest($request),
+            ExternalInviteRequest::class => $this->fromExternalInviteRequest($request),
         };
     }
 
@@ -30,6 +33,22 @@ class InvitationDtoFactory
         $request_data['creator'] = auth()->user();
 
         $request_data['expired_at'] = $request->date('expired_at', 'Y-m-d H:i');
+
+        return InvitationDto::from($request_data);
+    }
+
+    protected function fromExternalInviteRequest(ExternalInviteRequest $request): InvitationDto
+    {
+        $request_data = $request->validated();
+
+        $request_data['creator'] = $request->vacancy()->creator;
+
+        $request_data['interview_template_id'] = $request->validated(
+            'interview_template_id',
+            $request->vacancy()->interview_template_id
+        );
+
+        $request_data['should_be_invited_at'] = Carbon::parse($request->vacancy()->started_at);
 
         return InvitationDto::from($request_data);
     }
